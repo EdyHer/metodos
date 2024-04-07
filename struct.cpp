2fa38b0aa35d135f817957889df98e4687b7bb27
edy <edyehl15@gmail.com>
2024-04-07 00:34:32 -0600

    metodo

--- struct.cpp
+++ struct.cpp
@@ -25,8 +25,35 @@ main(){
 	}
 	cout<<"_________________ingreso de notas ___________________"<<endl;
 	for (int i=0;i<fila;i++){
-		cout<<
+		cout<<"codigo["<<i<<"]:";
+		cin>>estudiante.codigo[i];
+		cin.ignore();
+		cout<<"nombre completo["<<i<<"]:";
+		getline(cin,estudiante.nombre[i]);
 		for (int ii=0;ii<columna;ii++){
+			cout<<"ingrese nota["<<ii<<"] :";
+			cin>>*(*(estudiante.notas+i)+ii);
+		}
+		cout<<"________________________"<<endl;
+	}
+	cout<<"_____________mostar datos___________"<<endl;
+	
+		for (int i=0;i<fila;i++){
+		cout<<"codigo["<<i<<"]:"<<estudiante.codigo[i]<<endl;
+		cout<<"nombre completo["<<i<<"]:"<<estudiante.nombre[i]<<endl;
+		for (int ii=0;ii<columna;ii++){
+			cout<<"ingrese nota["<<ii<<"] :"<<*(*(estudiante.notas+i)+ii)<<endl;
+		}
+		cout<<"________________________"<<endl;
+	}
+	
+	for (int i=0;i<fila;i++){
+		delete[] estudiante.notas[i];
+	}
+		delete[] estudiante.codigo;
+	delete[] estudiante.nombre;
+	delete[] estudiante.notas;
+
 	system("pause");
 	
 }
